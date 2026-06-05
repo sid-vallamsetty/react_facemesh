c3d8dde1bbf1ca247828435b91239db0721e1d80
Sidharth Vallamsetty <sid.vallamsetty@gmail.com>
2026-06-05 18:25:00 -0400

    Webcam setup and initialize Facemesh function done

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import * as tf from '@tensorflow/tfjs';
+import * as facemesh from '@tensorflow-models/facemesh';
+import Webcam from 'react-webcam';
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam ref = {webcamRef} style = {
+          {  
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }
+        }/>
+      
+        <canvas ref = {canvasRef} style = {
+          {  
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }
+        }/>
+      </header>
+    </div>
+  )
+}
+
+export default App
