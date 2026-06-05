ba9727dc19e38004cc9afc853763b0541538c5c0
Sidharth Vallamsetty <sid.vallamsetty@gmail.com>
2026-06-05 19:48:33 -0400

    Face tracking now shows up in console log

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,45 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+  const runFacemesh = async () => {
+    const net = await facemesh.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.8,
+    });
+
+    setInterval(() => {
+      detect(net)
+    }, 100)
+  }
+
+  const detect = async (net) => {
+    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video.readyState === 4) {
+      // Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // set video properties
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // set canvas properties
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const face = await net.estimateFaces(video);
+      console.log(face);
+    }
+  }
+
+  runFacemesh();
+
   return (
     <div className="App">
       <header className="App-header">
-        <Webcam ref = {webcamRef} style = {
+        <Webcam 
+        ref = {webcamRef} 
+        style = {
           {  
             position: "absolute",
             marginLeft: "auto",
@@ -29,7 +64,9 @@ function App() {
           }
         }/>
       
-        <canvas ref = {canvasRef} style = {
+        <canvas 
+        ref = {canvasRef} 
+        style = {
           {  
             position: "absolute",
             marginLeft: "auto",
